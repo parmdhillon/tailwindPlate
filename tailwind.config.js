eafd5caec86d9f57e7770267ec2a0a4c4925f655
Parminder <iparm1996@gmail.com>
2021-09-29 22:19:25 -0400

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./public/**/*.html', './public/**/*.js', './public/**/*.php'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
